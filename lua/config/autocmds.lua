e0eb964ab770ccfaf45f548a8ecacc716d5dd7f7
joe <joenikushimi@protonmail.com>
2025-07-23 02:25:44 +0100

    Adding autocmd for plugin autoupdate

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -31,3 +31,17 @@ vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 		vim.diagnostic.open_float(nil, { focus = false })
 	end,
 })
+
+--- autocmd to automatically update plugins with lazy.vim
+local function augroup(name)
+	return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	group = augroup("autoupdate"),
+	callback = function()
+		require("lazy").update({
+			show = false,
+		})
+	end,
+})
